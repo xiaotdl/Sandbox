7809d16cd37eff4e3db09af3c5ed433715e64cf5
xiaotdl <xiaotdl@gmail.com>
2015-12-18 23:08:31 -0800

    update

--- perl/test_hash.pl
+++ perl/test_hash.pl
@@ -25,3 +25,38 @@ my %dump =
         hashgrep { $a != 1 && $b != 4 }
         %hash;
 print Dumper(\%dump);
+
+
+# == Hash Slice ==
+# ref: http://www.webquills.net/web-development/perl/perl-5-hash-slices-can-replace.html
+my %hash = (one => 1, two => 2, three => 3);
+my @keys = qw/one two three/;
+# Regular access to scalar key
+print "$hash{one}"; # 1
+print "\n";
+# Hash slice accesses multiple keys. Note the '@'
+print "@hash{@keys}"; # 1 2 3
+print "\n";
+
+# Hash slice assignment to multiple keys. Note the '@'
+@hash{@keys} = (11, 12, 13);
+print "@hash{@keys}"; # 11 12 13
+print "\n";
+
+
+my %number_for = (one => 1, two => 2, three => 3);
+my $num_for = \%number_for;
+my @columns = qw/three one two/;
+# Common syntax for dereferencing and getting a scalar index
+print $num_for->{one}; # 1
+print "\n";
+# Alternate syntax, the lazy way:
+print $$num_for{two}; # 2
+print "\n";
+print @$num_for{@columns}; # 312  <= this looks good!
+print "\n";
+# Alternate syntax, the explicit way
+print ${$num_for}{three}; # 3
+print "\n";
+print @{$num_for}{@columns}; # 312
+print "\n";
