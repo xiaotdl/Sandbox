5b0f3b7bce6b51900052ebfa66877e89d9ec6bd9
xiaotdl <xiaotdl@gmail.com>
2016-01-07 16:49:50 -0800

    update

--- shell/test_p4_change_client_view.sh
+++ shell/test_p4_change_client_view.sh
@@ -1,32 +1,54 @@
 #!/bin/bash
-# Example usage:
-# modifies $add_to_depot to include files you would like to include
-# $ ./change_client_view.sh xili-vm
-#
-# To apply changes on all branches:
-# $ p4 clients -e 'MASTER-*-FIT' | cut -d' ' -f2 > /tmp/clients.txt
-# $ while read -r client || [[ -n "$client"  ]]; do ./change_client_view.sh $client; done < /tmp/clients.txt
 
-p4client=$1
-add_to_depot="-(//depot/.*?/tm_lib/cec/... .*)"
-#remove_from_depot="(//depot/.*?/tm_lib/cec/... .*)"
+add_or_remove=$1
+file_pattern=$2
+p4client=$3
 tmp_file="/tmp/p4client.$p4client.$$"
 
+usage () {
+    cat <<"EOF";
+Usage:
+    $ ./change_client_view.sh add "//depot/.*?/tm_lib/cec/... .*" <p4_client_name>
+    $ ./change_client_view.sh remove "//depot/.*?/tm_lib/cec/... .*" <p4_client_name>
 
-export P4CLIENT=$p4client
+    To apply changes on all branches:
+    $ p4 clients -e 'MASTER-*-FIT' | cut -d' ' -f2 > /tmp/clients.txt
+    $ while read -r client || [[ -n "$client"  ]]; do ./change_client_view.sh add "//depot/.*?/tm_lib/cec/... .*" $client; done < /tmp/clients.txt
+EOF
+}
 
-p4 client -o > $tmp_file
-
-if [ -n "$add_to_depot"  ]; then
-    echo perl -i -pe "s|$add_to_depot|\1|" $tmp_file;
+if [[ "$add_or_remove" = "add" ]]; then
+    add_to_depot="-($file_pattern)"
+elif [[ "$add_or_remove" = "remove" ]]; then
+    remove_from_depot="($file_pattern)"
+else
+    usage
+    exit 1
 fi
 
-if [ -n "$remove_from_depot"  ]; then
-    echo perl -i -pe "s|$remove_from_depot|-\1|" $tmp_file
+if [[ "$#" -lt 3 ]]; then
+    usage
+    exit 1
 fi
 
-p4 client -i < $tmp_file
+main () {
+    export P4CLIENT=$p4client
 
-rm $tmp_file
+    p4 client -o $p4client> $tmp_file
 
-# p4 sync
+    if [ -n "$add_to_depot"  ]; then
+        perl -i -pe "s|$add_to_depot|\1|" $tmp_file;
+    fi
+
+    if [ -n "$remove_from_depot"  ]; then
+        perl -i -pe "s|$remove_from_depot|-\1|" $tmp_file
+    fi
+
+    p4 client -i < $tmp_file
+
+    rm $tmp_file
+
+    # p4 sync
+}
+
+main
