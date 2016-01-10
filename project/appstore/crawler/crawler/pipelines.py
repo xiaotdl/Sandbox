f94c55b7504cad3deec08a8c53f54eb461f1346a
xiaotdl <xiaotdl@gmail.com>
2016-01-10 14:35:32 -0800

    init

---
+++ project/appstore/crawler/crawler/pipelines.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+class CrawlerPipeline(object):
+    def  __init__(self):
+        self.file = open('appstore.dat', 'wb')
+
+    def process_item(self, item, spider):
+        # val = '{0}\t{1}\t{2}\n'.format(item['appid'], item['title'], item['intro'])
+        val = '{0}\t{1}\t{2}\n'.format(item['appid'], item['title'], item['recommended'])
+
+        self.file.write(val)
+        return item
