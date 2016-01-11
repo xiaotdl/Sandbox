7b88d86df17ee5a39f63cf83074122ab6097dcf9
xiaotdl <xiaotdl@gmail.com>
2016-01-10 22:29:35 -0800

    update

--- python/test_import_in_runtime.py
+++ python/test_import_in_runtime.py
@@ -3,9 +3,11 @@ import sys
 old_paths = list(sys.path)
 sys.path.append('lib')
 new_paths = list(sys.path)
-print [path for path in new_paths if path not in old_paths]
+print [path for path in set(new_paths) if path not in set(old_paths)]
+print [set(new_paths) ^ set(old_paths)]
 # >>>
 # ['lib']
+# [set(['lib'])]
 
 from test_module import print_hello
 print_hello()
