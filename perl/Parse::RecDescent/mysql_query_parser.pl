cd46748f53b8a9e9d77ab13b89d84471a8da9262
xiaotdl <xiaotdl@gmail.com>
2016-04-11 23:22:21 -0700

    update

--- perl/Parse::RecDescent/mysql_query_parser.pl
+++ perl/Parse::RecDescent/mysql_query_parser.pl
@@ -5,16 +5,70 @@ use warnings;
 
 use Parse::RecDescent;
 use Data::TreeDumper;
+use Data::Dumper;
 
-$::RD_HINT = 1;
+$::RD_HINT = 1;  # show detailed analyses and hints on both errors and warnings.
 
 my $grammar = q {
 
-expression:     <leftop: term ('+' | '-') term>
+# MySQL Operator Precedence
+# Ref: http://dev.mysql.com/doc/refman/5.7/en/operator-precedence.html
+# == highest precedence ==
+#    INTERVAL
+#    BINARY, COLLATE
+#    !
+#    - (unary minus), ~ (unary bit inversion)
+#    ^
+#    *, /, DIV, %, MOD
+#    -, +
+#    <<, >>
+#    &
+#    |
+#    = (comparison), <=>, >=, >, <=, <, <>, !=, IS, LIKE, REGEXP, IN
+#    BETWEEN, CASE, WHEN, THEN, ELSE
+#    NOT
+#    AND, &&
+#    XOR
+#    OR, ||
+#    = (assignment), :=
+# == lowest precedence ==
+
+
+# == lowest precedence ==
+
+expression:     or_expr
+
+or_expr:        and_expr m/(?: \|\| | or)/ix or_expr
+                         { $return = Mysql::Op::Or->new($item[1], $item[3]) }
+              | and_expr
+
+and_expr:       not_expr m/(?: && | and)/ix and_expr
+                         { $return = Mysql::Op::And->new($item[1], $item[3]) }
+              | not_expr
+
+not_expr:       m/(?: ! | not)/ix compare_expr
+                         { $return = Mysql::Op::Not->new($item[2]) }
+              | compare_expr
+              | value
+              | variable
+
+compare_expr:   binop_expr ('=' | '!=' | '<' | '>' | '<=' | '>=') binop_expr
                          {
-                             my $s = shift @{$item [1]};
-                             while (@{$item [1]}) {
-                                 my ($op, $t) = splice @{$item [1]}, 0, 2;
+                             my $op = $item[2];
+                             if    ($op eq '=' ) {$return = Mysql::Op::Eq->new($item[1], $item[3])}
+                             elsif ($op eq '!=') {$return = Mysql::Op::Ne->new($item[1], $item[3])}
+                             elsif ($op eq '<' ) {$return = Mysql::Op::Lt->new($item[1], $item[3])}
+                             elsif ($op eq '>' ) {$return = Mysql::Op::Gt->new($item[1], $item[3])}
+                             elsif ($op eq '<=') {$return = Mysql::Op::Le->new($item[1], $item[3])}
+                             elsif ($op eq '>=') {$return = Mysql::Op::Ge->new($item[1], $item[3])}
+                         }
+              | binop_expr
+
+binop_expr:     <leftop: term ('+' | '-') term>
+                         {
+                             my $s = shift @{$item[1]};
+                             while (@{$item[1]}) {
+                                 my ($op, $t) = splice @{$item[1]}, 0, 2;
                                  if    ($op eq '+') {$s = Mysql::Op::Add->new($s, $t)}
                                  elsif ($op eq '-') {$s = Mysql::Op::Subtract->new($s, $t)}
                              }
@@ -23,9 +77,9 @@ expression:     <leftop: term ('+' | '-') term>
 
 term:           <leftop: factor ('*' | '/' | '%') factor>
                          {
-                             my $t = shift @{$item [1]};
-                             while (@{$item [1]}) {
-                                 my ($op, $f) = splice @{$item [1]}, 0, 2;
+                             my $t = shift @{$item[1]};
+                             while (@{$item[1]}) {
+                                 my ($op, $f) = splice @{$item[1]}, 0, 2;
                                  if    ($op eq '*') {$t = Mysql::Op::Multiply->new($t, $f)}
                                  elsif ($op eq '/') {$t = Mysql::Op::Divide->new($t, $f)}
                                  elsif ($op eq '%') {$t = Mysql::Op::Modulo->new($t, $f)}
@@ -34,46 +88,79 @@ term:           <leftop: factor ('*' | '/' | '%') factor>
                          }
 
 factor:         functioncall
-              | number
+              | value
               | variable
-              | '+' factor                   {$return = $item [2]}
-              | '-' factor                   {$return = $item [2] * -1}
-              | '(' expression ')'           {$return = $item [2]}
+              | '+' factor                   {$return = $item[2]}
+              | '-' factor                   {$return = $item[2] * -1}
+              | '(' expression ')'           {$return = $item[2]}
 
 functioncall:   m/(?: sum | max | min)/ix '(' expression ')'
                          {
-                             if (lc $item[1] eq 'sum') {
-                                 $return = Mysql::Function::Sum->new($item[3]);
-                             }
-                             elsif (lc $item[1] eq 'max') {
-                                 $return = Mysql::Function::Max->new($item[3]);
-                             }
-                             elsif (lc $item[1] eq 'min') {
-                                 $return = Mysql::Function::Min->new($item[3]);
-                             }
+                             if    (lc $item[1] eq 'sum') { $return = Mysql::Function::Sum->new($item[3]) }
+                             elsif (lc $item[1] eq 'max') { $return = Mysql::Function::Max->new($item[3]) }
+                             elsif (lc $item[1] eq 'min') { $return = Mysql::Function::Min->new($item[3]) }
                          }
 
-number:         /\d+/    { $return = Mysql::Value->new($item[1]) }
+value:          /\d+/    { $return = Mysql::Value->new($item[1]) }
+              | "'" /[a-zA-Z0-9_-]+/ "'"
+                         { $return = Mysql::Value->new($item[2]) }
+              | '"' /[a-zA-Z0-9_-]+/ '"'
+                         { $return = Mysql::Value->new($item[2]) }
 
 variable:       /[a-zA-Z_][a-zA-Z0-9_]*/
                          { $return = Mysql::Variable->new($item[1]) }
+
+# == highest precedence ==
 };
 
-my $parser=Parse::RecDescent->new($grammar);
-
 my $tree;
+my $parser=Parse::RecDescent->new($grammar);
 my $context = {
     variables => {
-        column_names => ["name", "sex", "age"],
         rows => [
-            {name => 'Tom', sex => 'M',age => 11},
-            {name => 'Joe', sex => 'M',age => 22},
-            {name => 'Marry', sex => 'F',age => 33},
+            {name => 'Tom',  sex => 'M',age => 11},
+            {name => 'Joe',  sex => 'M',age => 22},
+            {name => 'Mary', sex => 'F',age => 33},
         ]
     }
 };
 
-$tree = $parser->expression("sum(999 + 1 * 1 * 1 + 888 + age) + 1");
+# MySQL query e.g.
+# mysql> SELECT sum(age + 1 * 2 - 3) + 1 FROM table WHERE name = 'Tom' || name = 'Joe';
+# == WHERE parser ==
+$tree = $parser->expression("name = 'Tom' || name = 'Joe'");
+print DumpTree($tree);
+
+my @rows = @{$context->{variables}->{rows}};
+for my $row (@rows) {
+    my $row_context = {
+        variables => $row,
+    };
+    my $validationError = $tree->validate($row_context);
+    die "validationError: " . $validationError . "\n"
+        if $validationError;
+    my $selected = $tree->eval($row_context) ? 1 : 0;
+    print "'$row->{name}' selected -> $selected\n";
+}
+# >>>
+#     blessed in 'Mysql::Op::Or'
+#    |- left =  blessed in 'Mysql::Op::Eq'  [OH1]
+#    |  |- left =  blessed in 'Mysql::Variable'  [OH2]
+#    |  |  `- name = name  [S3]
+#    |  `- right =  blessed in 'Mysql::Value'  [OH4]
+#    |     `- value = Tom  [S5]
+#    `- right =  blessed in 'Mysql::Op::Eq'  [OH6]
+#       |- left =  blessed in 'Mysql::Variable'  [OH7]
+#       |  `- name = name  [S8]
+#       `- right =  blessed in 'Mysql::Value'  [OH9]
+#          `- value = Joe  [S10]
+#    'Tom' selected -> 1
+#    'Joe' selected -> 1
+#    'Marry' selected -> 0
+
+
+# == SELECT parser ==
+$tree = $parser->expression("sum(age + 1 * 2 - 3) + 1 ");
 print DumpTree($tree);
 
 my $validationError = $tree->validate($context);
@@ -81,30 +168,23 @@ die "validationError: " . $validationError . "\n"
     if $validationError;
 
 print $tree->eval($context);
-
-#>>>
-# blessed in 'Mysql::Op::Add'
-# |- left =  blessed in 'Mysql::Function::Sum'  [OH1]
-# |  `- expr =  blessed in 'Mysql::Op::Add'  [OH2]
-# |     |- left =  blessed in 'Mysql::Op::Add'  [OH3]
-# |     |  |- left =  blessed in 'Mysql::Op::Add'  [OH4]
-# |     |  |  |- left =  blessed in 'Mysql::Value'  [OH5]
-# |     |  |  |  `- value = 999  [S6]
-# |     |  |  `- right =  blessed in 'Mysql::Op::Multiply'  [OH7]
-# |     |  |     |- left =  blessed in 'Mysql::Op::Multiply'  [OH8]
-# |     |  |     |  |- left =  blessed in 'Mysql::Value'  [OH9]
-# |     |  |     |  |  `- value = 1  [S10]
-# |     |  |     |  `- right =  blessed in 'Mysql::Value'  [OH11]
-# |     |  |     |     `- value = 1  [S12]
-# |     |  |     `- right =  blessed in 'Mysql::Value'  [OH13]
-# |     |  |        `- value = 1  [S14]
-# |     |  `- right =  blessed in 'Mysql::Value'  [OH15]
-# |     |     `- value = 888  [S16]
-# |     `- right =  blessed in 'Mysql::Variable'  [OH17]
-# |        `- name = age  [S18]
-# `- right =  blessed in 'Mysql::Value'  [OH19]
-#    `- value = 1  [S20]
-#    5731
+# >>>
+#     blessed in 'Mysql::Op::Add'
+#    |- left =  blessed in 'Mysql::Function::Sum'  [OH1]
+#    |  `- expr =  blessed in 'Mysql::Op::Subtract'  [OH2]
+#    |     |- left =  blessed in 'Mysql::Op::Add'  [OH3]
+#    |     |  |- left =  blessed in 'Mysql::Variable'  [OH4]
+#    |     |  |  `- name = age  [S5]
+#    |     |  `- right =  blessed in 'Mysql::Op::Multiply'  [OH6]
+#    |     |     |- left =  blessed in 'Mysql::Value'  [OH7]
+#    |     |     |  `- value = 1  [S8]
+#    |     |     `- right =  blessed in 'Mysql::Value'  [OH9]
+#    |     |        `- value = 2  [S10]
+#    |     `- right =  blessed in 'Mysql::Value'  [OH11]
+#    |        `- value = 3  [S12]
+#    `- right =  blessed in 'Mysql::Value'  [OH13]
+#       `- value = 1  [S14]
+#    64
 
 
 package Mysql::Function::Numeric;
@@ -230,6 +310,161 @@ sub validate {
     my ($self, $context) = @_;
 
     return $self->{left}->validate($context) || $self->{right}->validate($context);
+}
+
+
+package Mysql::Op::And;
+
+use base 'Mysql::Op::Binary';
+
+sub eval {
+    my ($self, $context) = @_;
+
+    my ($lhs, $rhs) = $self->SUPER::eval($context);
+    return $lhs && $rhs;
+}
+
+
+package Mysql::Op::Or;
+
+use base 'Mysql::Op::Binary';
+
+sub eval {
+    my ($self, $context) = @_;
+
+    my ($lhs, $rhs) = $self->SUPER::eval($context);
+    return $lhs || $rhs;
+}
+
+
+package Mysql::Op::Not;
+
+sub new {
+    my ($class, $exp) = @_;
+
+    return bless {
+        exp => $exp,
+    }, $class;
+}
+
+sub eval {
+    my ($self, $context) = @_;
+
+    return !$self->{exp}->eval($context);
+}
+
+sub validate {
+    my ($self, $context) = @_;
+
+    return $self->{exp}->validate($context);
+}
+
+
+package Mysql::Op::Eq;
+
+use base 'Mysql::Op::Binary';
+
+use Scalar::Util qw(looks_like_number);
+
+sub eval {
+    my ($self, $context) = @_;
+
+    my ($lhs, $rhs) = $self->SUPER::eval($context);
+    return
+        &looks_like_number($lhs) && &looks_like_number($rhs) ?
+            $lhs == $rhs
+        :
+            $lhs eq $rhs;
+}
+
+
+package Mysql::Op::Ne;
+
+use base 'Mysql::Op::Binary';
+
+use Scalar::Util qw(looks_like_number);
+
+sub eval {
+    my ($self, $context) = @_;
+
+    my ($lhs, $rhs) = $self->SUPER::eval($context);
+    return
+        &looks_like_number($lhs) && &looks_like_number($rhs) ?
+            $lhs != $rhs
+        :
+            $lhs ne $rhs;
+}
+
+
+package Mysql::Op::Lt;
+
+use base 'Mysql::Op::Binary';
+
+use Scalar::Util qw(looks_like_number);
+
+sub eval {
+    my ($self, $context) = @_;
+
+    my ($lhs, $rhs) = $self->SUPER::eval($context);
+    return
+        &looks_like_number($lhs) && &looks_like_number($rhs) ?
+            $lhs < $rhs
+        :
+            $lhs lt $rhs;
+}
+
+
+package Mysql::Op::Gt;
+
+use base 'Mysql::Op::Binary';
+
+use Scalar::Util qw(looks_like_number);
+
+sub eval {
+    my ($self, $context) = @_;
+
+    my ($lhs, $rhs) = $self->SUPER::eval($context);
+    return
+        &looks_like_number($lhs) && &looks_like_number($rhs) ?
+            $lhs > $rhs
+        :
+            $lhs gt $rhs;
+}
+
+
+package Mysql::Op::Le;
+
+use base 'Mysql::Op::Binary';
+
+use Scalar::Util qw(looks_like_number);
+
+sub eval {
+    my ($self, $context) = @_;
+
+    my ($lhs, $rhs) = $self->SUPER::eval($context);
+    return
+        &looks_like_number($lhs) && &looks_like_number($rhs) ?
+            $lhs <= $rhs
+        :
+            $lhs le $rhs;
+}
+
+
+package Mysql::Op::Ge;
+
+use base 'Mysql::Op::Binary';
+
+use Scalar::Util qw(looks_like_number);
+
+sub eval {
+    my ($self, $context) = @_;
+
+    my ($lhs, $rhs) = $self->SUPER::eval($context);
+    return
+        &looks_like_number($lhs) && &looks_like_number($rhs) ?
+            $lhs >= $rhs
+        :
+            $lhs ge $rhs;
 }
 
 
