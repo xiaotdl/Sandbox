07baa66991a2cbd3ed4a3571377095fbd5108ddb
xiaotdl <xiaotdl@gmail.com>
2016-05-26 11:34:47 -0700

    udpate

--- perl/test_system.pl
+++ perl/test_system.pl
@@ -3,11 +3,20 @@
 use strict;
 use warnings;
 
-my $exit_code = system("python -c 'import sys; sys.exit(-1);'");
+use Capture::Tiny qw(:all);
+
+# my $cmd = q[perl -e '$? = 1;'];
+# my $cmd = q[perl -e 'exit -1;'];
+# my $cmd = q[perl -e 'DESTROY { die "some msg"; }; exit 2;']; # die doesn't print anything inside DESTROY
+my $cmd = q[perl -e '1/0;'];
+
+my ($merged_output, $exit_code) = capture_merged { system($cmd) };
 
 my $exit = $exit_code >> 8;
 my $core = $exit_code & 128;
 my $signal = $exit_code & 127;
 print "Error Code: $exit_code (exit=$exit, core=$core, signal=$signal)\n";
+print "capture_merged: " . $merged_output;
 # >>>
 # Error Code: 65280 (exit=255, core=0, signal=0)
+# capture_merged: Illegal division by zero at -e line 1.
