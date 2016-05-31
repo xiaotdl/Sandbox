41e7db5cfbd99c18945fbd8dadb43c70a0ffc4dd
xiaotdl <xiaotdl@gmail.com>
2016-05-31 11:48:31 -0700

    update

--- perl/test_splice.pl
+++ perl/test_splice.pl
@@ -7,17 +7,28 @@
 
 # == insert ==
 my @others = qw(SnowWhite Humbert);
+#               0   1      2     3      4      5     6
 my @dwarfs = qw(Doc Grumpy Happy Sleepy Sneezy Dopey Bashful);
-splice @dwarfs, 3, 0, @others;
-print "@dwarfs";
+splice @dwarfs, 3, 0, @others; # insert "SnowWhite Humbert" starting from index: 3
+print "@dwarfs\n";
 # >>>
 # Doc Grumpy Happy SnowWhite Humbert Sleepy Sneezy Dopey Bashful
 
 
 # == replace ==
 my @others = qw(SnowWhite Humbert);
+#               0   1      2     3      4      5     6
 my @dwarfs = qw(Doc Grumpy Happy Sleepy Sneezy Dopey Bashful);
-splice @dwarfs, 2, 4, @others;
+splice @dwarfs, 2, 4, @others; # replace "Happy Sleepy Sneezy Dopey" with "SnowWhite Humbert"
 print "@dwarfs\n";
 # >>>
 # Doc Grumpy SnowWhite Humbert Bashful
+
+
+# == remove an element in the middle of an array ==
+#               0   1      2     3      4      5     6
+my @dwarfs = qw(Doc Grumpy Happy Sleepy Sneezy Dopey Bashful);
+splice @dwarfs, 3, 1; # remove 'Sleepy', index: 3, length: 1
+print "@dwarfs\n";
+# >>>
+# Doc Grumpy Happy Sneezy Dopey Bashful
