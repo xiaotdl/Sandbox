12efcabcb66989c912ee368d56b5ab279ab34375
xiaotdl <xiaotdl@gmail.com>
2016-06-24 11:43:41 -0700

    update

--- perl/test_table.pl
+++ perl/test_table.pl
@@ -74,11 +74,15 @@ my $table = [
 
 &table_view($table);
 
+# TODO:
+# When there are too many cols that exceeds COL_WIDTH (say 80),
+# wrap them in multiple lines.
+
 
 # >>>
 # col1     col2 col3
 # -------- ---- ----
-# 1        2    3
-# 4-sfdsfs 5    6
-# 7        8    9
-# 10       11   12
+# 1ab         2    3
+# 4-sfdsfs    5    6
+#        7    8    9
+#       10   11   12
