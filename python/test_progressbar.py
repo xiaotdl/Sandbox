4b760f1ca0aa1486538b008140b576750f8ed8a6
xiaotdl <xiaotdl@gmail.com>
2016-07-26 18:16:53 -0700

    update

--- python/test_progressbar.py
+++ python/test_progressbar.py
@@ -8,31 +8,33 @@
 
 import sys, time
 
-class ProgressBar:
-    def __init__(self, count = 0, total = 0, width = 50):
-        self.count = count
-        self.total = total
-        self.width = width
-    def move(self):
-        self.count += 1
-    def log(self, s):
-        sys.stdout.write(' ' * (self.width + 9) + '\r')
-        sys.stdout.flush()
-        print s
-        progress = self.width * self.count / self.total
-        sys.stdout.write('{0:3}/{1:3}: '.format(self.count, self.total))
-        sys.stdout.write('#' * progress + '-' * (self.width - progress) + '\r')
-        if progress == self.width:
-            sys.stdout.write('\n')
-        sys.stdout.flush()
+# == Example1 ==
+# class ProgressBar:
+#     def __init__(self, count = 0, total = 0, width = 50):
+#         self.count = count
+#         self.total = total
+#         self.width = width
+#     def move(self):
+#         self.count += 1
+#     def log(self, s):
+#         sys.stdout.write(' ' * (self.width + 9) + '\r')
+#         sys.stdout.flush()
+#         print s
+#         progress = self.width * self.count / self.total
+#         sys.stdout.write('{0:3}/{1:3}: '.format(self.count, self.total))
+#         sys.stdout.write('#' * progress + '-' * (self.width - progress) + '\r')
+#         if progress == self.width:
+#             sys.stdout.write('\n')
+#         sys.stdout.flush()
+#
+# bar = ProgressBar(total = 10)
+# for i in range(10):
+#     bar.move()
+#     bar.log('We have arrived at: ' + str(i + 1))
+#     time.sleep(1)
 
-bar = ProgressBar(total = 10)
-for i in range(10):
-    bar.move()
-    bar.log('We have arrived at: ' + str(i + 1))
-    time.sleep(1)
 
-
+# == breakdown ==
 # how it goes all the way.
 # import sys, time
 
@@ -59,3 +61,22 @@ for i in range(10):
     # sys.stdout.flush()
     # time.sleep(1)
 
+class Example:
+    def run(self):
+        TIMEOUT = 2
+        PROGRESS_ICONS = ['-', '\\', '|', '/']
+
+
+        end = time.time() + TIMEOUT
+        cnt = 0
+        msg = "Processing sth " + '(expects %s seconds)' % TIMEOUT
+        while time.time() < end:
+            sys.stdout.write('\r' + msg + ' ... ' + PROGRESS_ICONS[cnt % 4])
+            sys.stdout.flush()
+            cnt += 1
+            time.sleep(0.2)
+        else:
+            sys.stdout.write('\r' + msg + ' ... ' + '[DONE]')
+            sys.stdout.flush()
+
+Example().run()
