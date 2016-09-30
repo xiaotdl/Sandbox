aed1a42ed0242f640cb638d5bcceb17d9d6bd465
xiaotdl <xiaotdl@gmail.com>
2016-09-29 17:55:28 -0700

    update

--- python/test_subprocess.py
+++ python/test_subprocess.py
@@ -14,23 +14,23 @@ import subprocess
 
 # == Example 2 ==
 # run a cmd and get rc, stdout, stderr
-# class Result(object):
+class Result(object):
 
-#     def __init__(self, cmd, rc, stdout, stderr):
-#         self.cmd= cmd
-#         self.rc = rc
-#         self.stdout = stdout
-#         self.stderr = stderr
+    def __init__(self, rc, stdout, stderr, cmd):
+        self.rc = rc
+        self.stdout = stdout
+        self.stderr = stderr
+        self.cmd= cmd
 
-#     def __str__(self):
-#         cutoff = 128
-#         if len(self.stdout) > cutoff:
-#             self.stdout = self.stdout[:cutoff] + '...'
-#         if len(self.stderr) > cutoff:
-#             self.stderr = self.stderr[:cutoff] + '...'
+    def __str__(self):
+        cutoff = 128
+        if len(self.stdout) > cutoff:
+            self.stdout = self.stdout[:cutoff] + '...'
+        if len(self.stderr) > cutoff:
+            self.stderr = self.stderr[:cutoff] + '...'
 
-#         return "Result: cmd=%(cmd)s -> " \
-#                "rc=%(rc)s, stdout=%(stdout)s, stderr=%(stderr)s" % {k: repr(self.__dict__[k]) for k in self.__dict__}
+        return "Result: cmd=%(cmd)s -> " \
+               "rc=%(rc)s, stdout=%(stdout)s, stderr=%(stderr)s" % {k: repr(self.__dict__[k]) for k in self.__dict__}
 
 
 # def execute(cmd, shell=False, ignore_error=False):
@@ -95,60 +95,60 @@ import subprocess
 # print out
 
 
-# == Example 4 ==
-# run a cmd and get rc, stdout, stderr
+# # == Example 4 ==
+# # run a cmd and get rc, stdout, stderr
 
-from subprocess import Popen, PIPE, STDOUT
+# from subprocess import Popen, PIPE, STDOUT
 
-def run(cmd):
-    print "== cmd: '%s' ==" % cmd
-    child = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, close_fds=True)
-    child.wait()
-    rc = child.returncode
-    out = child.stdout.read()
-    err = child.stderr.read()
-    print "rc:\n", rc
-    print "out:\n", out
-    print "err:\n", err
+# def run(cmd):
+#     print "== cmd: '%s' ==" % cmd
+#     child = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, close_fds=True)
+#     child.wait()
+#     rc = child.returncode
+#     out = child.stdout.read()
+#     err = child.stderr.read()
+#     print "rc:\n", rc
+#     print "out:\n", out
+#     print "err:\n", err
 
-cmds = (
-    'cal | grep 2016',
-    'ls *.py',
-    'echo 123 456',
-)
+# cmds = (
+#     'cal | grep 2016',
+#     'ls *.py',
+#     'echo 123 456',
+# )
 
-for cmd in cmds:
-    run(cmd)
+# for cmd in cmds:
+#     run(cmd)
 
-# == Example 5 ==
-# run cmd with timeout
-from threading import Thread
-class SubprocessTimeoutError(Exception):
-    pass
+# # == Example 5 ==
+# # run cmd with timeout
+# from threading import Thread
+# class SubprocessTimeoutError(Exception):
+#     pass
 
-# Ref: http://www.ostricher.com/2015/01/python-subprocess-with-timeout/
-def run_command_with_timeout(cmd, timeout_sec):
-    """Execute `cmd` in a subprocess and enforce timeout `timeout_sec` seconds.
+# # Ref: http://www.ostricher.com/2015/01/python-subprocess-with-timeout/
+# def run_command_with_timeout(cmd, timeout_sec):
+#     """Execute `cmd` in a subprocess and enforce timeout `timeout_sec` seconds.
 
-    Return subprocess exit code on natural completion of the subprocess.
-    Raise an exception if timeout expires before subprocess completes."""
-    proc = subprocess.Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, close_fds=True)
-    proc_thread = Thread(target=proc.wait)
-    proc_thread.start()
-    proc_thread.join(timeout_sec)
-    if proc_thread.is_alive():
-        # Process still running - kill it and raise timeout error
-        try:
-            proc.kill()
-        except OSError, e:
-            # The process finished between the `is_alive()` and `kill()`
-            return proc.returncode
-        # OK, the process was definitely killed
-        raise SubprocessTimeoutError('Process #%d killed after %f seconds' % (proc.pid, timeout_sec))
-    # Process completed naturally - return exit code
-    return proc.returncode
+#     Return subprocess exit code on natural completion of the subprocess.
+#     Raise an exception if timeout expires before subprocess completes."""
+#     proc = subprocess.Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, close_fds=True)
+#     proc_thread = Thread(target=proc.wait)
+#     proc_thread.start()
+#     proc_thread.join(timeout_sec)
+#     if proc_thread.is_alive():
+#         # Process still running - kill it and raise timeout error
+#         try:
+#             proc.kill()
+#         except OSError, e:
+#             # The process finished between the `is_alive()` and `kill()`
+#             return proc.returncode
+#         # OK, the process was definitely killed
+#         raise SubprocessTimeoutError('Process #%d killed after %f seconds' % (proc.pid, timeout_sec))
+#     # Process completed naturally - return exit code
+#     return proc.returncode
 
-run_command_with_timeout('sleep 5', 2)
+# run_command_with_timeout('sleep 5', 2)
 # >>>
 # Traceback (most recent call last):
 #   File "test.py", line 80, in <module>
@@ -165,39 +165,53 @@ run_command_with_timeout('sleep 5', 2)
 # otherwise the subprocess will be hanging.
 # Ref:
 # https://thraxil.org/users/anders/posts/2008/03/13/Subprocess-Hanging-PIPE-is-your-enemy/
-# import tempfile
+import logging
+import tempfile
 
-# def run(cmd, shell=True, ignore_stdout=False, ignore_stderr=False):
-#     logging.info("run: '%s' on <localhost>..." % cmd)
+def run(cmd, shell=True, large_buffer=False):
+    logging.info("run: '%s' on <localhost>..." % cmd)
 
-#     # subprocess.PIPE is a fixed size (64K) buffer,
-#     # process will be hanging if stdout exceeds this limit.
-#     stdout = tempfile.TemporaryFile() if ignore_stdout else subprocess.PIPE
-#     stderr = tempfile.TemporaryFile() if ignore_stderr else subprocess.PIPE
+    # subprocess.PIPE is a fixed size (64K) buffer,
+    # process will be hanging if stdout exceeds this limit.
+    if large_buffer:
+        stdout = tempfile.TemporaryFile()
+        stderr = tempfile.TemporaryFile()
+    else:
+        stdout = subprocess.PIPE
+        stderr = subprocess.PIPE
 
-#     child = subprocess.Popen(
-#         cmd,
-#         shell=shell,
-#         stdout=stdout,
-#         stderr=stderr,
-#         close_fds=True
-#     )
+    child = subprocess.Popen(
+        cmd,
+        shell=shell,
+        stdout=stdout,
+        stderr=stderr,
+        close_fds=False
+    )
 
-#     child.wait()
+    child.wait()
+    rc = child.returncode
+    if large_buffer:
+        stdout.seek(0)
+        stderr.seek(0)
+        out = stdout.read()
+        err = stderr.read()
+        stdout.close()
+        stderr.close()
+    else:
+        out = child.stdout.read()
+        err = child.stderr.read()
 
-#     rc = child.returncode
-#     out, err = 'n/a', 'n/a'
-#     if not ignore_stdout:
-#         out = child.stdout.read()
-#     if not ignore_stderr:
-#         err = child.stderr.read()
 
-#     r = Result(rc, out, err, cmd)
-#     logging.debug(str(r))
-#     if r.rc != 0:
-#         logging.warning("Non-zero return code!!! %s" % r)
-#         logging.debug(r.stdout)
-#         logging.debug(r.stderr)
-#     return r
+    r = Result(rc, out, err, cmd)
+    logging.debug(str(r))
+    if r.rc != 0:
+        logging.warning("Non-zero return code!!! %s" % r)
+        logging.debug(r.stdout)
+        logging.debug(r.stderr)
+    return r
 
-# run("echo 'large stdout and stderr'", ignore_stdout=True, ignore_stderr=True)
+s = ' what up ' * 5
+r = run("echo 'large stdout and stderr, %s'" % s)
+print r
+r = run("echo 'large stdout and stderr, %s'" % s, large_buffer=True)
+print r
