243a82fe2871c27a6853584051123e50b5ed8c71
xiaotdl <xiaotdl@gmail.com>
2016-09-29 17:55:16 -0700

    update

--- python/test_argparse.py
+++ python/test_argparse.py
@@ -5,11 +5,13 @@ parser = argparse.ArgumentParser(prog='test_argparse.py')
 
 parser.add_argument('-v', '--var', dest='var')
 parser.add_argument('--include-tests', dest='include_tests', nargs='+')
+parser.add_argument('--include-daemons', dest='include_daemons', nargs='+')
 
 args = parser.parse_args()
 
 print args.var
 print args.include_tests
+print args.include_daemons
 
 # >>>
 # $ ./test_argparse.py -v abc --include-tests 44 55 66
