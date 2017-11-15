bc7d00d4b8d954284e8ed2e36dd17a876b714181
Xiaotian Li <xiaotdl@gmail.com>
2017-11-15 14:14:09 -0800

    update

--- ruby/_rbvmomi/test_10.192.72.20_propertyCollector.rb
+++ ruby/_rbvmomi/test_10.192.72.20_propertyCollector.rb
@@ -1,6 +1,5 @@
 require 'rbvmomi'
 require 'pry'
-require 'awesome_print'
 
 def to_short_s(s, limit=100)
     if (s.to_s.size() > limit)
@@ -20,38 +19,40 @@ CLUSTER_HOST = '10.192.12.51'
 VIM = RbVmomi::VIM
 
 vim = VIM.connect(host: HOST, user: USER, password: PASS, insecure: true)
+
 dc = vim.serviceInstance.find_datacenter(DATACENTER) || fail("datacenter not found: #{DATACENTER}")
 compute = dc.find_compute_resource(COMPUTE)
-puts compute
 
 host = compute.host.select {|h| h.name.eql?(CLUSTER_HOST)}[0]
 puts host
 cm = host.configManager
 ns = cm.networkSystem
 
-propertyCollector = vim.propertyCollector
-# TODO Retreive distrubuted port groups, to allow distributed
-# port group to be selected when cloning.  See
-# Action::Clone.prepare_network_card_backing_info.
 
+propertyCollector = vim.propertyCollector
+## TODO Retreive distrubuted port groups, to allow distributed
+## port group to be selected when cloning.  See
+## Action::Clone.prepare_network_card_backing_info.
+#
 # #== retrieve host network info ==
 # filterSpec = VIM.PropertyFilterSpec(
 #   :objectSet => [{
-#     :obj => host,
-#     :skip => true,
-#     :selectSet => [
-#       VIM.TraversalSpec(
-#         :type => 'HostSystem',
-#         :path => 'configManager.networkSystem',
-#         :skip => false,
-#       )
-#     ]
+#	 :obj => host,
+#	 :skip => true,
+#	 :selectSet => [
+#	   VIM.TraversalSpec(
+#		 :type => 'HostSystem',
+#		 :path => 'configManager.networkSystem',
+#		 :skip => false,
+#	   )
+#	 ]
 #   }],
 #   :propSet => [{
-#     :type => 'HostNetworkSystem',
-#     :pathSet => %w(networkInfo.vswitch networkInfo.portgroup),
+#	 :type => 'HostNetworkSystem',
+#	 :pathSet => %w(networkInfo.vswitch networkInfo.portgroup),
 #   }]
 # )
+#r = propertyCollector.RetrieveProperties(:specSet => [filterSpec])
 
 # #== retrieve rootFolder's childEntity(datacenters) ==
 #filterSpec = VIM.PropertyFilterSpec(
@@ -78,14 +79,27 @@ propertyCollector = vim.propertyCollector
 #  :options => { }
 ##)
 
-dvportgroup = dc.network.find {|network| network.name.eql?("DHCP-AutoTest-Vlan-2130")}
-filterSpec = VIM.PropertyFilterSpec(
-  :objectSet=>[{:obj=>dvportgroup}],
-  :propSet=>[{:type=>"DistributedVirtualPortgroup", :all=>true}]
-)
-
-r = propertyCollector.RetrieveProperties(:specSet => [filterSpec])
-puts dvportgroup
-puts r[0].propSet.map{|dynamicProp| "  #{dynamicProp.name} => "+to_short_s(dynamicProp.val)}
+   filterSpec = VIM.PropertyFilterSpec(
+	 :objectSet=>dc.network.map{|network| {:obj => network}},
+	 :propSet=>[{:type=>"DistributedVirtualPortgroup", :all=>true}]
+   )
+   
+   r = propertyCollector.RetrieveProperties(:specSet => [filterSpec])
+   #puts r
+   #puts r.class
+   #puts r.size
+   #puts r[0].propSet.map{|dynamicProp| "  #{dynamicProp.name} => "+to_short_s(dynamicProp.val)}
+   dvpg = []
+   r.each do |objectContent|
+	 dvpg << objectContent.obj
+	 puts "== #{objectContent.class}, #{objectContent.obj}, #{objectContent.obj.name} =="
+	 #if objectContent.obj.name.eql?("DHCP-AutoTest-Vlan-2130")
+	 #end
+	 #objectContent.propSet.each do |p|
+	 #  puts "  #{p.name} => "+to_short_s(p.val)
+	 #  #network_info[:portgroup].concat(p.val)
+	 #end
+   end
+#   puts dvpg
 
 puts 'eof';
