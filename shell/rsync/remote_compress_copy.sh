630be61c0de1adb9a48c61de3baa6da1f6317517
Xiaotian Li <xiaotdl@gmail.com>
2017-11-17 14:47:46 -0800

    Update remote_compress_copy.sh

--- shell/rsync/remote_compress_copy.sh
+++ shell/rsync/remote_compress_copy.sh
@@ -12,13 +12,14 @@ rsync -avzhe ssh --progress /home/files/ root@remoteip:/path/to/files/
 
 
 
-# Copy VM Template between ESX hosts
-~ # rsync -avzh --sparse --progress vmfs/volumes/ISCSI-Vol01/fit-t-bigiq-5.3.0/ root@10.192.197.22:/vmfs/volumes/ISCSI-Vol0/fit-t-bigiq-5.3.0.sparse
-
 # Download rsync static binary on ESX hosts
 https://wiki.contribs.org/Backup_of_ESXi_Virtual_Machines_using_Affa
 $ wget http://mirror.contribs.org/smeserver/contribs/michaelw/sme7/Affa2/affa-esxi-setup-02.tgz
 $ tar xvzf affa-esxi-setup-02.tgz
 $ scp bexi/rsync-static root@10.192.197.22:/bin/rsync
 $ ssh root@10.192.197.22 'chmod +x /bin/rsync'
+
+# Copy VM Template between ESX hosts
+~ # rsync -avzh --sparse --progress vmfs/volumes/ISCSI-Vol01/fit-t-bigiq-5.3.0/ root@10.192.197.22:/vmfs/volumes/ISCSI-Vol0/fit-t-bigiq-5.3.0.sparse
+
 
