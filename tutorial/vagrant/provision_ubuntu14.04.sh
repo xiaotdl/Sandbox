17999d1f59c39f3847a153d27952db3328667ef0
Xiaotian Li <xiaotdl@gmail.com>
2018-12-26 11:47:15 -0800

    Update provision_ubuntu14.04.sh

--- tutorial/vagrant/provision_ubuntu14.04.sh
+++ tutorial/vagrant/provision_ubuntu14.04.sh
@@ -1,8 +1,12 @@
 # https://www.vagrantup.com/intro/getting-started/
 # https://app.vagrantup.com/boxes/search
+
+# Find latest ubuntu distro from: https://wiki.ubuntu.com/Releases
+# e.g.
 # Ubuntu 14.04 LTS
 $ vagrant init "ubuntu/trusty64"
 # Ubuntu 16.04 LTS
 $ vagrant init "ubuntu/xenial64"
+
 $ vagrant up
 $ vagrant ssh
