45e9c90226eb18e3f9d198f2c143e20edb919485
xiaotdl <xiaotdl@gmail.com>
2019-03-28 00:14:26 -0700

    update

--- cpp/test/copy_ctor.cpp
+++ cpp/test/copy_ctor.cpp
@@ -1,27 +1,65 @@
 #include <iostream>
 
 using namespace std;
-struct A {
-  A(int val) : val(val) {}
 
-  A& operator=(A other) = delete;
-  int val = 100;
+struct B;
+
+struct A {
+  A() = default;
+
+  A(int val)
+  : b(make_unique<B>(val)) {
+  }
+
+  std::unique_ptr<B> b = nullptr;
 };
 
 struct B {
-  B(int val)
-  : a(make_unique<A>(val)) {
-  }
   B() {
+      cout << "ctor" << endl;
   }
 
-  std::unique_ptr<A> a = nullptr;
+  B(int val) : val(val) {
+      cout << "custom ctor" << endl;
+  }
+
+  B(const B& other) {
+      cout << "copy ctor" << endl;
+  }
+
+  B& operator=(const B& other) {
+      cout << "copy assignment" << endl;
+      return *this;
+  }
+
+  int val = 100;
 };
 
 // To Run: !g++ -std=c++14 -Wall % && ./a.out
 int main() {
-  B b(99);
-  cout << b.a->val << endl;
+  A a(99);
+  cout << a.b->val << endl;
+
+  cout << "B b(100);" << endl;
+  B b(100);
+  auto bup = std::make_unique<B>(100);
+
+  cout << "B b1(b);" << endl;
+  B b1(b);  // XXX: copy ctor:   B(const B& other) = delete;
+  auto b1up = std::make_unique<B>(*bup);
+
+  cout << "B b2;" << endl;
+  B b2;     // XXX: ctor:        B() = delete; 
+  auto b2up = std::make_unique<B>();
+
+  cout << "b2 = b;" << endl;
+  b2 = b;   // XXX: copy assign: B& operator=(const B& other) = delete;
+  *b2up = *bup;
+
+  cout << "B b3 = b;" << endl;
+  B b3 = b; // XXX: copy ctor:   B(const B& other) = delete;
+  auto b3up = std::make_unique<B>(*bup);
+
   cout << "EOP" << endl;
 }
 
