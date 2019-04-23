781848dbea6ba9ff324c1c7aa06769c33d2fe566
Xiaotian Li <xiaotdl@gmail.com>
2019-04-22 18:41:39 -0700

    update

---
+++ python/_requests/test.py
@@ -0,0 +1,32 @@
+import requests
+
+# reference: https://2.python-requests.org//en/master/
+
+# an example to use python requests library to:
+#   1. send a http request
+#   2. receives a http response and do some work with it
+
+URL = "https://jsonplaceholder.typicode.com/todos/1"
+# >>>
+# {
+#   "userId": 1,
+#   "id": 1,
+#   "title": "delectus aut autem",
+#   "completed": false
+# }
+#
+
+resp = requests.get(URL)
+print(resp.status_code)
+
+print(resp.headers)
+# >>> 'Content-Type': 'application/json; charset=utf-8'
+
+print(resp.text) # raw http body
+
+json_resp = resp.json() # serialize raw http body into json format
+print(json_resp["userId"])
+print(json_resp["id"])
+print(json_resp["title"])
+print(json_resp["completed"])
+
